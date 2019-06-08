d3a3686d95b38d62635ebc078b8e15d8fe6cb6c3
vera98x <v.schoonderwoerd@outlook.com>
2019-06-08 15:10:55 +0200

    updated calculation

--- QT/pidController.cpp
+++ QT/pidController.cpp
@@ -90,26 +90,23 @@ void PIDController::calculateError(const Vec2i &ballPos) {
     set_point[1] = set_point[1] - zero[1];
 
     float ballOnMotorK= 
-            ( ball[0]*motor[0] + ball[1] * motor[1] ) / sqrt(pow(motor[0], 2) + pow(motor[1], 2));
+            ( ball[0]*motor[0] + ball[1] * motor[1] ) / (pow(motor[0], 2) + pow(motor[1], 2));
     );
 
-    BallOnLine[0] = ball[0] - (ballOnMotorK * (motor[1])); //This is the projected point on the line that goes between the motor and the center of the 3 motors.
-    BallOnLine[1] = ball[1] + (ballOnMotorK * (motor[0])); //This is the projected point on the line that goes between the motor and the center of the 3 motors.
+    BallOnLine[0] = (ballOnMotorK * (motor[0])); //This is the projected point on the line that goes between the motor and the center of the 3 motors.
+    BallOnLine[1] = (ballOnMotorK * (motor[1])); //This is the projected point on the line that goes between the motor and the center of the 3 motors.
 
 
     Vec2f SetpointOnLine;
      float setpointOnMotorK= 
-            ( set_point[0]*motor[0] + set_point[1] * motor[1] ) / sqrt(pow(motor[0], 2) + pow(motor[1], 2));
+            ( set_point[0]*motor[0] + set_point[1] * motor[1] ) / (pow(motor[0], 2) + pow(motor[1], 2));
     );
 
-    float setpointOnMotorKx = setpointOnMotorK * motor[0];
-    float setpointOnMotorKy = setpointOnMotorK * motor[1];
+    SetpointOnLine[0] = setpointOnMotorK * (motor[0]);
+    SetpointOnLine[1] = setpointOnMotorK * (motor[1]);
 
-    SetpointOnLine[0] = setpoint[0] - (setpointOnMotorKx * (motor[1]));
-    SetpointOnLine[1] = setpoint[1] + (setpointOnMotorKy) * (motor[0]));
-
-    float distanceSetpointCenter = getDistanceTo(center_point, SetpointOnLine);
-    float distanceBallCenter = getDistanceTo(center_point, BallOnLine);
+    float distanceSetpointCenter = getDistanceTo(motor, SetpointOnLine);
+    float distanceBallCenter = getDistanceTo(motor, BallOnLine);
 
     current_error = distanceSetpointCenter - distanceBallCenter;
     */
