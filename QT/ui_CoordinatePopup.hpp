e681a4bbc08381452b72b20e8f1e98cbfaee19c7
Florian Humblot <florian.humblot@student.hu.nl>
2019-06-19 11:00:58 +0200

    Added docstrings to the coordinate popup class.

--- QT/ui_CoordinatePopup.hpp
+++ QT/ui_CoordinatePopup.hpp
@@ -15,26 +15,66 @@ using namespace cv;
 class ui_coordinate_popup : public QWidget {
 Q_OBJECT
 public:
+    /**
+     * Construct a popup widget
+     * @param parent the main window
+     */
     explicit ui_coordinate_popup(QWidget *parent = nullptr);
 
+    /**
+     * @brief method for the main window to specify the target whose coordinates are being set.
+     */
     void setTargetText(const std::string &target);
 
+    /**
+     * Set the current coordinates in a Label
+     * @param currentCoords
+     */
     void setCurrentCoordinates(Vec2i currentCoords);
 
+    /**
+     * Show a still so that the user knows where to click.
+     * @param image
+     */
     void setCameraImage(const Mat &image);
 
+    /**
+     * override the event filter to capture only mouseclicks that happen on the image.
+     * @param obj
+     * @param event
+     * @return
+     */
     bool eventFilter(QObject *obj, QEvent *event) override;
 
 public slots:
+
+    /**
+     * Event handler for the 'confirm' button
+     */
     void on_confirm_btn_clicked();
+
+    /**
+     * Event handler for the 'cancel' button
+     */
     void on_cancel_btn_clicked();
 signals:
+
+    /**
+     * Signal the main window that there is a new coordinate.
+     * @param targetCoordinate
+     */
     void image_coordinate(Vec2i targetCoordinate);
 
 protected:
 
 private:
+    /**
+     * The generated ui so that QT knows what to draw and where.
+     */
     Ui::Form ui;
+    /**
+     * Store the coordinate as it's being updated.
+     */
     Vec2i currentCoords = {0, 0};
 };
 
